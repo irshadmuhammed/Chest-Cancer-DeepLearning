3aacaae6d835e697ee505124ae4ccb2420b781dd
irshadmuhammed <irshadmm16@gmail.com>
2025-02-27 19:16:57 +0530

    mlflow integration cimplete

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from ChestCancerDetection import logger
+from ChestCancerDetection.components.model_evaluation import Evaluation
+from ChestCancerDetection.config.configuration import ConfigurationManager
 from ChestCancerDetection.pipeline.satge_1_data_ingestion import DataIngestionTrainingPipeline
 from ChestCancerDetection.pipeline.stage_2_prepare_base_model import PrepareBaseModelTrainingPipeline
+from ChestCancerDetection.pipeline.stage_3_train_model import ModelTrainingPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 
@@ -25,3 +28,27 @@ try:
 except Exception as e:
     logger.exception(e)
     raise e
+
+
+STAGE_NAME = "Model Training"
+try:
+    logger.info(f"*******************")
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = ModelTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = "Model Evaluation  "
+try:
+    config = ConfigurationManager()
+    eval_config = config.get_evaluation_config()
+    evaluation = Evaluation(eval_config)
+    evaluation.evaluation()
+    evaluation.log_into_mlflow()
+
+except Exception as e:
+   raise e
